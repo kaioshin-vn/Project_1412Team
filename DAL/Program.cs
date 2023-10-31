8b6d1d6b5d1e7168630c82087498d04fdd06b110
Admin <Admin@MSI>
2023-10-31 22:12:02 +0700

    check push project files.

---
+++ DAL/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Program
+    {
+        static void WriteToScreen()
+        { // hàm không có tham số
+            Console.WriteLine("Ham khong co tham so");
+        }
+
+        static void Main()
+        {
+            WriteToScreen();
+
+            Console.ReadKey(); // dừng màn hình
+        }
+    }
+}
