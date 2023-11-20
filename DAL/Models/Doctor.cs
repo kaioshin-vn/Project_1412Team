04633c61edd244dc21862c5bb466a200b717a8bb
HQuanGd03 <hoangquan.nguyenxhxt@gmail.com>
2023-11-20 17:18:34 +0700

    sua

---
+++ DAL/Models/Doctor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Doctor
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required, Range(0, 1)]
+        public  int Gender { get; set; } //giới tính
+        [Required, Phone]
+        public string NumberPhone { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public bool visible { get; set; } // ẩn hiện
+
+        // quan hệ
+        public virtual Staff Staff { get; set; }
+        //public virtual StatusDoctor StatusdDoctor { get; set; }
+        //public virtual MedicaBill MedicaBill { get; set; }
+    }
+}
---
+++ DAL/Models/TimeKeeping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class TimeKeeping
+    {
+        public Guid IdStaff { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        // quan hệ
+        public virtual Staff Staff { get; set; }
+    }
+}
