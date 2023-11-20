94c71cf8347d672fd1ed9ecf0ea0750ad4f277dc
Dang Giap <dgiap540@gmail.com>
2023-11-20 11:14:38 +0700

    aa

--- PRL/Form1.cs
+++ PRL/Form1.cs
@@ -6,5 +6,25 @@ namespace PRL
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Location = new Point(button1.Right - 25, button1.Top + 5);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
