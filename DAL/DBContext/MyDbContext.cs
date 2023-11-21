1fe54a52a92e456c6e6af8b147e3100b7674ee37
Dang Giap <dgiap540@gmail.com>
2023-11-21 23:42:44 +0700

    fixdb

--- DAL/DBContext/MyDbContext.cs
+++ DAL/DBContext/MyDbContext.cs
@@ -22,7 +22,7 @@ namespace DAL.DBContext
         public DbSet<Service> Services { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=Duan1;Integrated Security=True");
+            optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=DuAn1;Integrated Security=True");
         }
         //protected override void OnModelCreating(ModelBuilder modelBuilder)
         //{
