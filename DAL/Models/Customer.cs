4430b693be5b69c9932f352e0cb568b982e25a36
Trọng Tấn <tantt241102@gmail.com>
2023-11-21 14:41:03 +0700

    next

---
+++ DAL/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(15)]
+        public string Phone { get; set; }
+        [StringLength(100)]
+        public string Address { get; set; }
+        [StringLength(5)]
+        public string Sex { get; set; }
+        public bool Visible { get; set; }
+
+        // quan hệ
+        public virtual ICollection<Bill> Bills { get; set; }
+        //public virtual ICollection<MedicalBill> MedicalBills { get; set; }
+
+
+    }
+}
--- DAL/Models/Statiscal.cs
+++ DAL/Models/Statiscal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,13 @@ namespace DAL.Models
     public class Statiscal
     {
         public Guid Id { get; set; }
+        public Statis Type { get; set; }
         public string Note { get; set; }
+
+
+        // quan hệ
+        public virtual Bill Bill { get; set; }
+        public virtual Salary Salary { get; set; }
+
     }
 }
