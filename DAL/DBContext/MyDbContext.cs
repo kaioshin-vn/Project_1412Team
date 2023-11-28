55c2a8607d9d94d8264840a75d7a80cdca738438
thọ thọ <thovvph31698@fpt.edu.vn>
2023-11-28 22:24:06 +0700

    dbset

--- DAL/DBContext/MyDbContext.cs
+++ DAL/DBContext/MyDbContext.cs
@@ -17,24 +17,24 @@ namespace DAL.DBContext
         public MyDbContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Admin> Admins { get; set; }
-        public DbSet<HoaDon> Bills { get; set; }
-        public DbSet<HoaDonChiTiet> BillDetails { get; set; }
-        public DbSet<DanhGia> Rates { get; set; }
-        public DbSet<LichSuKham> HealtRecords { get; set; }
-        public DbSet<DichVu> Services { get; set; }
-        public DbSet<Phong> Clinics { get; set; }
-        public DbSet<KhachHang> Customers { get; set; }
-        public DbSet<BacSi> Doctors { get; set; }
-        public DbSet<PhieuKham> MedicalBills { get; set; }
-        public DbSet<ThongBao> Notices { get; set; }
-        public DbSet<YTa> Nurses { get; set; }
-        public DbSet<Luong> Salaries { get; set; }
-        public DbSet<CaKham> Shifts { get; set; }
-        public DbSet<NhanVien> Staffs { get; set; }
-        public DbSet<ThongKe> Statiscals { get; set; }
-        public DbSet<TrangThaiPhong> StatusClinics { get; set; }
-        public DbSet<TrangThaiNhanVien> StatusDoctors { get; set; }
-        public DbSet<ChamCong> TimeKeepings { get; set; }
+        public DbSet<HoaDon> HoaDons { get; set; }
+        public DbSet<HoaDonChiTiet> HoaDonChiTiets { get; set; }
+        public DbSet<DanhGia> DanhGias { get; set; }
+        public DbSet<LichSuKham> lichSuKhams { get; set; }
+        public DbSet<DichVu> DichVus { get; set; }
+        public DbSet<Phong> Phongs { get; set; }
+        public DbSet<KhachHang> KhachHangs { get; set; }
+        public DbSet<BacSi> BacSis { get; set; }
+        public DbSet<PhieuKham> PhieuKhams { get; set; }
+        public DbSet<ThongBao> ThongBaos { get; set; }
+        public DbSet<YTa> YTas { get; set; }
+        public DbSet<Luong> Luongs { get; set; }
+        public DbSet<CaKham> CaKhams { get; set; }
+        public DbSet<NhanVien> NhanViens { get; set; }
+        public DbSet<ThongKe> ThongKes { get; set; }
+        public DbSet<TrangThaiPhong> TrangThaiPhongs { get; set; }
+        public DbSet<TrangThaiNhanVien> TrangThaiNhanViens { get; set; }
+        public DbSet<ChamCong> ChamCongs { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=DuAn1;Integrated Security=True");
