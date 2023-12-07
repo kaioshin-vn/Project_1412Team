46b5705c238bad6939b0646f454ee5e1276c1f00
Dang Giap <dgiap540@gmail.com>
2023-12-07 22:30:32 +0700

    ttt

--- PRL/Program.cs
+++ PRL/Program.cs
@@ -13,6 +13,7 @@ namespace PRL
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            //giap
             Application.Run(new Login());
         }
     }
